c2ecca47e73e1a4bfa2a473d0b816771938e54cc
Denise <119461139+Denise1536@users.noreply.github.com>
2023-07-05 21:30:02 -0400

    Initial commit

---
+++ Controllers/CoffeeShopRegistrationController.cs
@@ -0,0 +1,33 @@
+﻿using CoffeeShopRegistration.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopRegistration.Controllers
+{
+    public class CoffeeShopRegistrationController : Controller
+    { 
+        private List<User> users = new List<User>();
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Registration(User user)
+        {
+            return RedirectToAction("Summary", user);
+        }
+
+        public IActionResult Summary(User user)
+         {
+            return View(user);
+         }
+
+    }
+}
